14728439a07e20a867fc3a5381c4a0a3589b5b95
Aurélien <aurelien.domino@gmail.com>
2021-12-06 00:23:00 +0100

    Fully broken omg

--- src/main/java/ru/meloncode/xmas/utils/LocationUtils.java
+++ src/main/java/ru/meloncode/xmas/utils/LocationUtils.java
@@ -10,17 +10,17 @@ public class LocationUtils {
     }
 
     public static long getChunkKey(Location location) {
-        return getChunkKey(location.getBlockX(), location.getBlockZ());
+        return getChunkKey(location.getBlockX() >> 4, location.getBlockZ() >> 4);
     }
 
     public static long getChunkKey(int x, int z) {
-        x = floor(x) >> 4;
-        z = floor(z) >> 4;
+        x = floor(x);
+        z = floor(z);
         return (((long) x) << 32) | (z & 0xFFFFFFFFL);
     }
 
     public static int floor(double num) {
-        int floor = (int) num;
+        final int floor = (int) num;
         return floor == num ? floor : floor - (int) (Double.doubleToRawLongBits(num) >>> 63);
     }
 
